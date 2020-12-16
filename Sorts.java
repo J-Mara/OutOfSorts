970f5c7b9fe92f47f29ce493ba00e73c53e95d7d
J-Mara <jmaragakis20@stuy.edu>
2020-12-16 13:30:29 -0500

    Sorts class with bubble sorting

--- Sorts.java
+++ Sorts.java
@@ -1,8 +1,4 @@
 public class Sorts{
-  /**Bubble sort of an int array.
-  *@postcondition The array will be modified such that the elements will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
   public static void bubbleSort(int[] data){
     boolean sorted = false;
     while(sorted == false){
